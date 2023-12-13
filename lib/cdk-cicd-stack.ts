b8b39c04d8f8610fd51e9177d974e7aad3beab07
Walter Wu <walterwu_001@hotmail.com>
2023-12-13 14:11:39 +1100

    Update branch

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('walterwu001/cdk-cicd', 'cicd-practice'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+    });
+    
   }
 }
